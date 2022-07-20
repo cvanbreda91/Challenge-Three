c98d869a45d336a6dc42dbae68b10526409a7d14
Courtney Van Breda <cmvanbreda91@gmail.com>
2022-07-20 14:36:07 -0400

    working password code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,38 @@
 // Assignment code here
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWZYZ";
-var specialCharacters ="~`!@#$%^&*()-_+={}[]|:;'<>,.?";
-var numbers ="1234567890";
-//var length="";
-
 
 function generatePassword(length = 8) {
-  let password = '';
-  let passwordLength = length;
- 
-  let array = [];
-  for (let i=0; i<length; i++){
-    array.push(Math.floor(Math.random() * lowerCase.length)); // Random index values corresponding to the character set are pushed to array
+    let password = '';
+    let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let passwordLength = length;
+   
+    let array = [];
+    for (let i=0; i<length; i++){
+      array.push(Math.floor(Math.random() * chars.length)); // Random index values corresponding to the character set are pushed to array
+    }
+   
+    for (let i = 0; i < passwordLength; i++) {
+      password += chars[array[i]]; // No division-remainder operator now necessary (values already correspond to indexes of chars)
+    }
+   
+    const msg = `Your new password is: "${password}"`
+    console.log(msg);
+    return password;
   }
- 
-  for (let i = 0; i < passwordLength; i++) {
-    password += lowerCase[array[i]]; // No division-remainder operator now necessary (values already correspond to indexes of chars)
-  }
- 
-  return password;
-}
- 
-generatePassword(); 
-// Get references to the #generate element
+   
+  generatePassword(8);
+
+//Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+//Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+passwordText.value = password;
 }
 
-// Add event listener to generate button
+//Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
