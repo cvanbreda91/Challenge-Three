6b3f5eb47ef9fe991dd72b71bee8998d2ea8c2a0
Courtney Van Breda <cmvanbreda91@gmail.com>
2022-07-20 17:08:51 -0400

    code working correctly with user buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 // Assignment code here
 
-function generatePassword(length = 8) {
+function generatePassword(length=20) {
+  
+  if ((document.getElementById('UpperCase').checked) && (document.getElementById('Number').checked) && (document.getElementById('Character').checked)){
     let password = '';
-    let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*()abcdefghijklmnopqrstuvwxyz";
     let passwordLength = length;
    
     let array = [];
@@ -16,10 +18,132 @@ function generatePassword(length = 8) {
    
     const msg = `Your new password is: "${password}"`
     console.log(msg);
-    return password;
-  }
+    return password;}
+  
+else if ((document.getElementById('UpperCase').checked) && (document.getElementById('Number').checked)){
+    let password = '';
+    let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789abcdefghijklmnopqrstuvwxyz";
+    let passwordLength = length;
    
-  generatePassword(8);
+    let array = [];
+    for (let i=0; i<length; i++){
+      array.push(Math.floor(Math.random() * chars.length)); // Random index values corresponding to the character set are pushed to array
+    }
+   
+    for (let i = 0; i < passwordLength; i++) {
+      password += chars[array[i]]; // No division-remainder operator now necessary (values already correspond to indexes of chars)
+    }
+   
+    const msg = `Your new password is: "${password}"`
+    console.log(msg);
+    return password;}
+    else if ((document.getElementById('UpperCase').checked) && (document.getElementById('Character').checked)){
+      let password = '';
+      let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^^&*()abcdefghijklmnopqrstuvwxyz";
+      let passwordLength = length;
+     
+      let array = [];
+      for (let i=0; i<length; i++){
+        array.push(Math.floor(Math.random() * chars.length)); // Random index values corresponding to the character set are pushed to array
+      }
+     
+      for (let i = 0; i < passwordLength; i++) {
+        password += chars[array[i]]; // No division-remainder operator now necessary (values already correspond to indexes of chars)
+      }
+     
+      const msg = `Your new password is: "${password}"`
+      console.log(msg);
+      return password;}
+      else if ((document.getElementById('Number').checked) && (document.getElementById('Character').checked)){
+        let password = '';
+        let chars = "1234567890!@#$%^^&*()abcdefghijklmnopqrstuvwxyz";
+        let passwordLength = length;
+       
+        let array = [];
+        for (let i=0; i<length; i++){
+          array.push(Math.floor(Math.random() * chars.length)); // Random index values corresponding to the character set are pushed to array
+        }
+       
+        for (let i = 0; i < passwordLength; i++) {
+          password += chars[array[i]]; // No division-remainder operator now necessary (values already correspond to indexes of chars)
+        }
+       
+        const msg = `Your new password is: "${password}"`
+        console.log(msg);
+        return password;}
+  else if (document.getElementById('Number').checked){
+    let password = '';
+    let chars = "0123456789abcdefghijklmnopqrstuvwxyz";
+    let passwordLength = length;
+   
+    let array = [];
+    for (let i=0; i<length; i++){
+      array.push(Math.floor(Math.random() * chars.length)); // Random index values corresponding to the character set are pushed to array
+    }
+   
+    for (let i = 0; i < passwordLength; i++) {
+      password += chars[array[i]]; // No division-remainder operator now necessary (values already correspond to indexes of chars)
+    }
+   
+    const msg = `Your new password is: "${password}"`
+    console.log(msg);
+    return password;}
+    else if (document.getElementById('Character').checked){
+      let password = '';
+      let chars = "!@#$%^&*()abcdefghijklmnopqrstuvwxyz";
+      let passwordLength = length;
+     
+      let array = [];
+      for (let i=0; i<length; i++){
+        array.push(Math.floor(Math.random() * chars.length)); // Random index values corresponding to the character set are pushed to array
+      }
+     
+      for (let i = 0; i < passwordLength; i++) {
+        password += chars[array[i]]; // No division-remainder operator now necessary (values already correspond to indexes of chars)
+      }
+     
+      const msg = `Your new password is: "${password}"`
+      console.log(msg);
+      return password;}
+      else if (document.getElementById('UpperCase').checked){
+        let password = '';
+        let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        let passwordLength = length;
+       
+        let array = [];
+        for (let i=0; i<length; i++){
+          array.push(Math.floor(Math.random() * chars.length)); // Random index values corresponding to the character set are pushed to array
+        }
+       
+        for (let i = 0; i < passwordLength; i++) {
+          password += chars[array[i]]; // No division-remainder operator now necessary (values already correspond to indexes of chars)
+        }
+       
+        const msg = `Your new password is: "${password}"`
+        console.log(msg);
+        return password;}
+  else {
+    let password = '';
+    let chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()";
+    let passwordLength = length;
+   
+    let array = [];
+    for (let i=0; i<length; i++){
+      array.push(Math.floor(Math.random() * chars.length)); // Random index values corresponding to the character set are pushed to array
+    }
+   
+    for (let i = 0; i < passwordLength; i++) {
+      password += chars[array[i]]; // No division-remainder operator now necessary (values already correspond to indexes of chars)
+    }
+   
+    const msg = `Your new password is: "${password}"`
+    console.log(msg);
+    return password;}
+
+  }
+  
+   
+  generatePassword();
 
 //Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
